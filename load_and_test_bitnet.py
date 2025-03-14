ea1621365515da6de4c7dc5e0b8d311a0f6befcc
Eric Liu <eliu2002@stanford.edu>
2025-03-14 13:53:50 -0700

    clean up

--- load_and_test_bitnet.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, PreTrainedTokenizerFast
-
-tokenizer = PreTrainedTokenizerFast(tokenizer_file="Bitnet B1 Tokenizer.json")
-model = AutoModelForCausalLM.from_pretrained("1bitLLM/bitnet_b1_58-large", trust_remote_code=True)
